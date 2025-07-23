14757265f467fb7c6df0d9e5c41e6a3014659696
Ankur Auti <ankurauti@gmail.com>
2025-07-23 16:01:47 +0530

    fixed errors

--- services/api.ts
+++ services/api.ts
@@ -47,7 +47,7 @@ export interface EventResponse {
 
 // Axios instance
 const api: AxiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api/v1',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://nepal-api.indirex.io',
   headers: {
     'Content-Type': 'application/json',
   },
